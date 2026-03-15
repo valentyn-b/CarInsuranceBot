f788d52f6e13992ec95ae152c81830ca523ad3fc
Valentyn B. <valentyn.basiuk.work@gmail.com>
2026-03-15 10:59:53 +0200

    feat: add Telegram bot foundation and DI setup

---
+++ CarInsuranceBot.API/Configuration/TelegramSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace CarInsuranceBot.API.Configuration
+{
+    public class TelegramSettings
+    {
+        public string BotToken { get; set; } = string.Empty;
+    }
+}
---
+++ CarInsuranceBot.API/Controllers/BotController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+
+namespace CarInsuranceBot.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        private readonly ITelegramBotClient _botClient;
+
+        public BotController(ITelegramBotClient botClient)
+        {
+            _botClient = botClient;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] object update)
+        {
+            // Test
+            Console.WriteLine($"Incoming update: {update}");
+            return Ok();
+        }
+    }
+}
